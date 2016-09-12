fd8b6276ecac4fa17c26580ef577c32d9ca582c5
Rikke Rye-Holmboe <rrhdesign@hotmail.com>
2016-09-12 13:54:51 +0200

    Fixed missing implementations in crypto.cpp, updated tests

--- crypto.hpp
+++ crypto.hpp
@@ -1,5 +1,6 @@
 #include <openssl/evp.h>
 #include <openssl/sha.h>
+#include <openssl/md5.h>
 #include <sstream>
 #include <iomanip>
 #include <string>
@@ -22,7 +23,14 @@ public:
  
   /// Return the MD5 (128-bit) hash from input.
   static std::string md5(const std::string &input, size_t iterations = 1) {
-    throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(128 / 8);
+    MD5((const unsigned char *)input.c_str(), input.size(), (unsigned char *)hash.c_str());
+
+    for (size_t c = 1; c < iterations; ++c)
+      MD5((const unsigned char *)hash.c_str(), hash.size(), (unsigned char *)hash.c_str());
+
+    return hash;
   }
 
   /// Return the SHA-1 (160-bit) hash from input.
@@ -39,12 +47,26 @@ public:
 
   /// Return the SHA-256 (256-bit) hash from input.
   static std::string sha256(const std::string &input, size_t iterations = 1) {
-    throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(256 / 8);
+    SHA256((const unsigned char *)input.c_str(), input.size(), (unsigned char *)hash.c_str());
+
+    for (size_t c = 1; c < iterations; ++c)
+      SHA256((const unsigned char *)hash.c_str(), hash.size(), (unsigned char *)hash.c_str());
+
+    return hash;
   }
 
   /// Return the SHA-512 (512-bit) hash from input.
   static std::string sha512(const std::string &input, size_t iterations = 1) {
-    throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(512 / 8);
+    SHA512((const unsigned char *)input.c_str(), input.size(), (unsigned char *)hash.c_str());
+    
+    for (size_t c = 1; c < iterations; ++c)
+      SHA512((const unsigned char *)hash.c_str(), hash.size(), (unsigned char *)hash.c_str());
+    
+    return hash;
   }
 
   /// Return key from the Password-Based Key Derivation Function 2 (PBKDF2).
--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -4,9 +4,14 @@
 using namespace std;
 
 int main() {
+  //
   // Results copied from https://caligatio.github.io/jsSHA/
   assert(Crypto::hex(Crypto::sha1("Test")) == "640ab2bae07bedc4c163f679a746f7ab7fb5d1fa");
   assert(Crypto::hex(Crypto::sha1("Test", 2)) == "af31c6cbdecd88726d0a9b3798c71ef41f1624d5");
+  
+  assert(Crypto::hex(Crypto::sha256("Test")) == "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25");
+  
+  assert(Crypto::hex(Crypto::sha512("Test")) == "c6ee9e33cf5c6715a1d148fd73f7318884b41adcb916021e2bc0e800a5c5dd97f5142178f6ae88c8fdd98e1afb0ce4c8d2c54b5f37b30b7da1997bb33b0b8a31");
 
   // Results copied from http://www.freecodeformat.com/pbkdf2.php
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 4096, 128 / 8)) == "f66df50f8aaa11e4d9721e1312ff2e66");
